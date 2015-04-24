cbc688f7de70f8a16955f72dff71fda2e96a42a1
Nicholas Harrigan <sparrigan@gmail.com>
2015-04-24 14:24:46 -0700

    Got autolayout working with font-resizing funciton in AutoLayoutTest project (in practise files)

--- Practise_files/AutoLayoutTest/AutoLayoutTest/ViewController.swift
+++ Practise_files/AutoLayoutTest/AutoLayoutTest/ViewController.swift
@@ -13,153 +13,47 @@ class ViewController: UIViewController {
             let view1 = UIView()
     let metricsDictionary = ["view1Height": 50.0, "viewWidth":100.0 ]
     var titleText:UITextView = UITextView()
-    let button   = UIButton.buttonWithType(UIButtonType.System) as! UIButton
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     
-
+        //NOTE: Is important to define textview in viewDidLoad so that it exists for the
+        //first call to viewDidLayoutSubviews (which is called even before first rotation)
+        //otherwise get an error
+        titleText.setTranslatesAutoresizingMaskIntoConstraints(false)
+        titleText.text = "Ready, steady, teddy beddy ready breaky scooby dooby doo and all that jazz and the like. Who is your favourite piece of lint? Mine too! We were made for each other. Clearly."
+        titleText.font = UIFont(name: "Arial", size: 200)
+        UIFont.preferredFontForTextStyle(UIFontTextStyleHeadline)
+        titleText.editable = false
+        titleText.backgroundColor = UIColor.redColor()
         
-        
-
-    
-
-        view1.setTranslatesAutoresizingMaskIntoConstraints(false)
-        view1.backgroundColor = UIColor.redColor()
-    
-        
-                view.addSubview(view1)
-        
-        
-        //addConstrationsFunc()
-        
-    
     }
     
     override func viewDidAppear(animated: Bool) {
+        //Function to setup autolayout constraints.
+        //NOTE: Is important that this is called from viewDidAppear as opposed to
+        //from viewDidLoad, beause otherwise
         testWithText()
     }
     
-    func addConstrationsFunc() {
-        
-        let viewsDictionary = ["view1":view1,"MainView":self.view]
-        
-        
-        
-        
-        
-        //Can do constraints using 'constraintsWithVisualFormat' method of NSLayoutConstraint that allows you to use Visual Formatting Language (VFL) to enter constraints.
-        let view1_constraint_H:Array = NSLayoutConstraint.constraintsWithVisualFormat("H:[view1(viewWidth)]", options: NSLayoutFormatOptions(0), metrics: metricsDictionary, views: viewsDictionary)
-        let view1_constraint_V:Array = NSLayoutConstraint.constraintsWithVisualFormat("V:[view1(view1Height)]", options: NSLayoutFormatOptions(0), metrics: metricsDictionary, views: viewsDictionary)
-        
-
-        //Or can do constraints using a stadnard base call to NSLayoutConstraint, where pass specific details of constraint (init by specifying relation to another view)
-        
-        //self.view.addConstraint(NSLayoutConstraint(item: view1, attribute: .Width, relatedBy: .Equal, toItem: self.view, attribute: .Width, multiplier: 0.75, constant: 0))
-       //Try and do width with VFL
-        let viewTest:Array = NSLayoutConstraint.constraintsWithVisualFormat("H:[view1(==MainView)]", options: NSLayoutFormatOptions(0), metrics: metricsDictionary, views: viewsDictionary)
-        
-        self.view.addConstraint(NSLayoutConstraint(item: view1, attribute: .CenterX, relatedBy: .Equal, toItem: self.view, attribute: .CenterX, multiplier: 1, constant: 0))
-        
-        self.view.addConstraint(NSLayoutConstraint(item: view1, attribute: .Top, relatedBy: .Equal, toItem: self.view, attribute: .Top, multiplier: 1, constant: 50))
-        
-        //In either case, we could either addConstraint directly to the view that is superview to the two we are specifying a relation between. Or we could assign the constraint to a variable and then add it this way.
-        
-        self.view.addSubview(view1)
-        
-        //view1.addConstraints(view1_constraint_H)
-        self.view.addConstraints(viewTest)
-        self.view.addConstraints(view1_constraint_V)
-        //view1.addConstraint(leftConstraint)
-        
-        println("first check width: \(view1.frame.size.width)")
-        println("first check height: \(view1.frame.size.height)")
-        
-    }
+    
     
     
     override func viewDidLayoutSubviews() {
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        //Old stuff
-        /*
-        
-        //println("first check width!: \(titleText.frame.size.width)")
-        //println("first check height!: \(titleText.frame.size.height)")
-        //println("Font size: \(titleText.font.pointSize)")
-        println("Contentsize height: \(titleText.intrinsicContentSize().height)")
-        println("Frame height: \(titleText.frame.size.height)")
-        println("")
-        while (titleText.intrinsicContentSize().height > titleText.frame.size.height && titleText.font.pointSize > 5)
-        {
+        //When rotation etc... of ipads, run function that checks text
+        //fits into boxes
+        sizeFontToView(titleText,maxFontSize: 500,minFontSize: 5)
 
-            titleText.font = UIFont(name: titleText.font.fontName, size: titleText.font.pointSize-1)
-//                [self.font fontWithSize:self.font.pointSize -1];
-            println("Font size: \(titleText.font.pointSize)")
-            println("Contentsize height: \(titleText.contentSize.height)")
-            println("Frame height: \(titleText.frame.size.height)")
-            println("")
-        }
-         //titleText.layoutSubviews()
-        /*
-        var fontSize:CGFloat = 500
-        titleText.font = UIFont(name: "Arial", size: fontSize)
-        var minSize:CGFloat = 5
-        var ii: Int = 0
-        while (fontSize > minSize && titleText.sizeThatFits(CGSizeMake(titleText.frame.size.width,200000)).height >= titleText.frame.size.height) {
-            println("in loop: \(ii)")
-            ii++
-            fontSize -= 1
-            titleText.font = UIFont(name: "Arial", size: fontSize)
-        }
-*/
-        */
-  
-        
-        
     }
 
 
     
     func testWithText() {
         
-
-        //Textfield
-        titleText.setTranslatesAutoresizingMaskIntoConstraints(false)
-        titleText.text = "Ready, steady, teddy beddy ready breaky"
-        titleText.font = UIFont(name: "Arial", size: 200)
-        UIFont.preferredFontForTextStyle(UIFontTextStyleHeadline)
-        titleText.editable = false
-        titleText.backgroundColor = UIColor.redColor()
-        
-        //titleText.contentOffset = CGPointZero
-        
-        
-        //Add views to main view
-        self.view.addSubview(button)
+        //Add views to main view - important to do this here
         self.view.addSubview(titleText)
-        
-        titleText.sizeToFit()
-        titleText.layoutIfNeeded()
-       
-        
 
-        
         //Dictionary of views to give constraints
         let viewsDictionary = ["titleText":titleText,"MainView":self.view]
         
@@ -167,30 +61,80 @@ class ViewController: UIViewController {
         let wConstraint = NSLayoutConstraint(item: titleText, attribute: .Width, relatedBy: .Equal, toItem: self.view, attribute: .Width, multiplier: 0.75, constant: 0)
         //Constraint on height and y position of text field
         let vConstraints:Array = NSLayoutConstraint.constraintsWithVisualFormat("V:|-50-[titleText(100)]", options: NSLayoutFormatOptions(0), metrics: nil, views: viewsDictionary)
-        //Center the textfield
         
+        //Center the textfield constraint
         let centConstraint = NSLayoutConstraint(item: titleText, attribute: .CenterX, relatedBy: .Equal, toItem: self.view, attribute: .CenterX, multiplier: 1, constant: 0)
 
-        //Seems to work if I MANUALLY SET THE SIZE OF THE TEXTBOX HERE
-        //titleText.frame = CGRectMake(0, 0, 20, 20)
-        
-        
         //Add constraints to superviews
         //(Note that .addConstraint and .addConstraints are different!)
         //self.view.addConstraint(wConstraint)
         
         self.view.addConstraints([wConstraint,centConstraint]+vConstraints)
-//self.view.addConstraint(wConstraint)
+    }
+    
+    
+    func sizeFontToView(tView:UITextView,maxFontSize:Double = 500,minFontSize:Double = 5) {
+        //maxFontSize is the Maximum font size you would consider allowable (defaults to 500)
+        //minFontSize is the Minumum font size you would consider allowable (defaults to 5)
         
+        //TO DO: TREAT TVIEW AS AN OPTIONAL AND UNWRAP CAREFULLY (AS MIGHT GET THIS CALLED
+        //BEFORE HAS BEEN ADDED TO VIEW ETC... IF IS DONE AFTER VIEWDIDLOAD)
         
+        //ALSO: FIRST CHECK WHETHER TEXT ALREADY FITS? TO STOP NEED FOR ITERATIONS IF FUNC
+        //IS CALLED ACCIDENTALLY WHEN IT ISN'T NEEDED?
         
-
-        /*
-        while (fontSize > minSize &&  [newView sizeThatFits:(CGSizeMake(newView.frame.size.width, FLT_MAX))].height >= newView.frame.size.height ) {
-            fontSize -= 1.0;
-            newView.font = [tv.font fontWithSize:fontSize];
+        //ALSO: DO BINARY SEARCH INSTEAD OF LINEAR
+        
+        //ALSO: NEED TO FIGURE OUT HOW TO DO WHEN HAVE TEXT CENTERED (I.E. HOW DO WE SET
+        //THIS OPTION IN boundingRectWithSize?
+        
+        //Max size for tallerSize (just needs to be very large)
+        let kMaxFieldHeight = 9999
+        //String that we want to try and get to fit
+        var fitString:NSString = tView.text
+        //Fudge factor due to padding in UIView
+        var fudgeFactor = 16.0
+        //Font size we start with
+        
+        //Start font off at largest size that we would allow
+        var fontSize = maxFontSize
+        
+        //println(tView.text)
+ 
+        tView.font = UIFont(name: tView.font.fontName, size: CGFloat(fontSize))
+        
+        //Make holder size that is width of the view but very tall - so we can see what height
+        //text has when we try and force it into it
+        var tallerSize = CGSizeMake(tView.frame.size.width - CGFloat(fudgeFactor), CGFloat(kMaxFieldHeight))
+        //Dictionary with font attributes in that we use to get size of text (of this font type/size) when pushed into tallerSize
+        var attrs = [NSFontAttributeName: UIFont(name: tView.font.fontName,size: CGFloat(fontSize)) as! AnyObject]
+        //Get first run of stringSize for current fontSize when try to push into tallerSize
+        var stringSize = fitString.boundingRectWithSize(tallerSize,
+            options: NSStringDrawingOptions.UsesLineFragmentOrigin,
+            attributes: attrs,
+            context: nil).size
+        
+        //Loop reducing font until will fit
+        while (stringSize.height >= tView.frame.size.height) {
+            
+            if (fontSize <= minFontSize) {
+                println("too small!!")
+                return
+            } else {
+                fontSize -= 1.0
+                
+                tView.font = UIFont(name: tView.font.fontName,size: CGFloat(fontSize))
+                
+                //Update the attributes dictionary with the new font size
+                var attrs = [NSFontAttributeName: UIFont.systemFontOfSize(CGFloat(fontSize))]
+                //Generate a new stringSize trying to fit this new sized font into tallerSize
+                stringSize = fitString.boundingRectWithSize(tallerSize,
+                    options: NSStringDrawingOptions.UsesLineFragmentOrigin,
+                    attributes: attrs,
+                    context: nil).size
+            }
+            
         }
-        */
         
     }
     
--- Practise_files/textResizeTest/textResizeTest/ViewController.swift
+++ Practise_files/textResizeTest/textResizeTest/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var titleText:UITextView = UITextView(frame: CGRectMake(0,0,300,100))
+    var tText:UITextView = UITextView(frame: CGRectMake(0,0,400,600))
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,47 +18,47 @@ class ViewController: UIViewController {
         
         
         //Textfield
-        titleText.setTranslatesAutoresizingMaskIntoConstraints(false)
-        titleText.text = "Ready, steady, teddy beddy ready breaky "
-        titleText.font = UIFont(name: "Arial", size: 200)
+        tText.setTranslatesAutoresizingMaskIntoConstraints(false)
+        tText.text = "Ready, steady, teddy beddy ready breaky"
+        tText.font = UIFont(name: "Arial", size: 200)
         UIFont.preferredFontForTextStyle(UIFontTextStyleHeadline)
-        titleText.editable = false
-        titleText.backgroundColor = UIColor.redColor()
+        tText.editable = false
+        tText.backgroundColor = UIColor.redColor()
 
-        self.view.addSubview(titleText)
+        self.view.addSubview(tText)
         
         //Auto resize text
         
-        autoResize()
+        sizeFontToView(tText,maxFontSize:500,minFontSize:5)
         
     }
 
     
-    func autoResize() {
+    func sizeFontToView(tView:UITextView,maxFontSize:Double = 500,minFontSize:Double = 5) {
         
-        
+        //maxFontSize is the Maximum font size you would consider allowable (defaults to 500)
+        //minFontSize is the Minumum font size you would consider allowable (defaults to 5)
         
         //Max size for tallerSize (just needs to be very large)
         let kMaxFieldHeight = 9999
         //String that we want to try and get to fit
-        var fitString:NSString = titleText.text
-        //Maximum font size you would consider
-        var maxFontSize = 500.0
-        //Minumum font size you would consider
-        var minFontSize = 5.0
+        var fitString:NSString = tView.text
         //Fudge factor due to padding in UIView
         var fudgeFactor = 16.0
         //Font size we start with
         
         //Start font off at largest size that we would allow
         var fontSize = maxFontSize
-        titleText.font = UIFont(name: titleText.font.fontName, size: CGFloat(fontSize))
-     
+        
+        
+        tView.font = UIFont(name: tView.font.fontName, size: CGFloat(fontSize))
+        
+        
         //Make holder size that is width of the view but very tall - so we can see what height
         //text has when we try and force it into it
-        var tallerSize = CGSizeMake(titleText.frame.size.width - CGFloat(fudgeFactor), CGFloat(kMaxFieldHeight))
+        var tallerSize = CGSizeMake(tView.frame.size.width - CGFloat(fudgeFactor), CGFloat(kMaxFieldHeight))
         //Dictionary with font attributes in that we use to get size of text (of this font type/size) when pushed into tallerSize
-        var attrs = [NSFontAttributeName: UIFont(name: titleText.font.fontName,size: CGFloat(fontSize)) as! AnyObject]
+        var attrs = [NSFontAttributeName: UIFont(name: tView.font.fontName,size: CGFloat(fontSize)) as! AnyObject]
         //Get first run of stringSize for current fontSize when try to push into tallerSize
         var stringSize = fitString.boundingRectWithSize(tallerSize,
             options: NSStringDrawingOptions.UsesLineFragmentOrigin,
@@ -66,7 +66,7 @@ class ViewController: UIViewController {
             context: nil).size
         
         //Loop reducing font until will fit
-        while (stringSize.height >= titleText.frame.size.height) {
+        while (stringSize.height >= tView.frame.size.height) {
         
             if (fontSize <= minFontSize) {
                 println("too small!!")
@@ -74,7 +74,7 @@ class ViewController: UIViewController {
             } else {
                 fontSize -= 1.0
                 
-                titleText.font = UIFont(name: titleText.font.fontName,size: CGFloat(fontSize))
+                tView.font = UIFont(name: tView.font.fontName,size: CGFloat(fontSize))
                 
                 //Update the attributes dictionary with the new font size
                 var attrs = [NSFontAttributeName: UIFont.systemFontOfSize(CGFloat(fontSize))]
